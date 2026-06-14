86789379d28dd4e166e2d33517da7c219baf9bd4
kustr-eedu <eduardokuster1@outlook.com>
2026-06-14 19:29:24 -0300

    Update initial.jsx
    Updates on 1.3.6:
    
    1. The GreetUser has been ported as a component instead of a website-only feature.

--- src/pages/initial.jsx
+++ src/pages/initial.jsx
@@ -2,36 +2,15 @@ import { useEffect, useState } from "react";
 import NewsCardSmall from "./components/newsCardSmall";
 import GrasswingFooter from "./components/footer";
 import WebsiteSelector from "./components/siteSelector";
+import GreetUser from "./components/greetUser"
 import "./stylesheet.css";
 
 function Initial() {
-  const [time, setTime] = useState();
-
-  useEffect(() => {
-    setInterval(() => {
-      const dateObject = new Date();
-
-      const hour = dateObject.getHours();
-      const minutes = dateObject.getMinutes();
-      const seconds = dateObject.getSeconds();
-
-      const currentTime = hour + ":" + minutes + ":" + seconds;
-
-      setTime(currentTime);
-    }, 1000);
-  }, []);
-
   return (
     <div class="main-container">
       <WebsiteSelector />
       <section />
-      <div class="greet-user">
-        <div class="greet-user-text">
-          <text>Hello there, [username]</text>
-          <section />
-          <text>Your time is: {time}</text>
-        </div>
-      </div>
+      <GreetUser />
       <section />
       <NewsCardSmall />
       <section />
